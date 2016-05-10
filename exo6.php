060fafd90e21de2707d9d1939bf548df0aa4273e
maryse-caron60400 <maryse.caron3@wanadoo.fr>
2016-05-10 14:16:03 +0200

    corr exo6

---
+++ exo6.php
@@ -0,0 +1,15 @@
+  <?php
+  $tableau = array("HTML", "CSS", "Javascript", "PHP");
+  function tri($langage){
+  sort ($langage);
+  return $langage;}
+  $triretour = array();
+  $triretour = tri($tableau);
+  foreach($triretour as $retour){
+    echo $retour;
+  }
+  function fusiontableau($fusionlang){
+    return implode(" ; ",$fusionlang);
+  }
+  echo fusiontableau($tableau);
+   ?>
